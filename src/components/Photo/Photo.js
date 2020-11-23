3169f8c626a60025d36c310caf80555befc33e5d
Patrick Shriwise <pshriwise@gmail.com>
2020-11-23 15:16:05 -0600

    Enabling use of google photos.

--- src/components/Photo/Photo.js
+++ src/components/Photo/Photo.js
@@ -11,11 +11,18 @@ export default function Photo(props) {
 
   const onClick = props.history.goBack;
 
+  let img_src = "";
+
+  if (photoId.includes(".jpg")) {
+      img_src = `/images/${tripId}/photos/${photoId}`;
+  } else {
+      img_src = `https://drive.google.com/uc?export=view&id=${photoId}`;
+  }
+
   return (
     <Fragment>
       <Overlay onClick={onClick} className="Photo-overlay" zIndex={300} />
-      <img onClick={onClick} className="Photo"
-        src={`/images/${tripId}/photos/${photoId}.jpg`} />
+      <img onClick={onClick} className="Photo" src={`${img_src}`} />
     </Fragment>
   );
 }
--- src/components/Thumbnail/Thumbnail.js
+++ src/components/Thumbnail/Thumbnail.js
@@ -4,12 +4,21 @@ import PropTypes from 'prop-types';
 import './Thumbnail.scss';
 
 export default function Thumbnail({ id, caption, tripId }) {
+
+    let img_src = "";
+
+    if (id.includes(".jpg")) {
+        img_src = `/images/${tripId}/thumbs/${id}`;
+    } else {
+        img_src = `https://drive.google.com/uc?export=view&id=${id}`;
+    }
+
   return (
-    <div className="Thumbnail">
-      <img className="Thumbnail-image" src={`/images/${tripId}/thumbs/${id}.jpg`}
-        alt={caption} />
-      {caption && <p className="Thumbnail-caption">{caption}</p>}
-    </div>
+          <div className="Thumbnail">
+          <img className="Thumbnail-image" src={`${img_src}`}
+      alt={caption} />
+          {caption && <p className="Thumbnail-caption">{caption}</p>}
+      </div>
   );
 }
 
